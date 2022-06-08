7653b0ad66899d0b56990a412459e50c821b2a8b
DUC1 PHUONG TRUNG <phgtrungduc@gmail.com>
2022-06-08 17:49:30 +0700

    khoi tao validate bang vee-valide

--- Code/WhereHouse/Frontend/where-house/src/main.js
+++ Code/WhereHouse/Frontend/where-house/src/main.js
@@ -4,11 +4,13 @@ import router from './router/index.js'
 import vuetify from './plugins/vuetify'
 import  '@/mixin/index'
 import store from './store/index';
+import { ValidationProvider } from 'vee-validate';
 ;// import axios from 'axios' 
 Vue.config.productionTip = false
 // window.axios = require('axios');
 
 Vue.prototype.$baseURL = 'https://localhost:44304/';
+Vue.component('ValidationProvider', ValidationProvider);
 
 new Vue({
   router,
